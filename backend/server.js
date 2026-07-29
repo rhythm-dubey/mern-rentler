162ec7febab094551d60127689be9283d2951719
Rhythm Dubey <rhythm@itradicals.com>
2026-07-29 14:23:57 +0530

    Updates Wired backend with frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -11,7 +12,17 @@ app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.APP_PORT || 7000;
 
+const __dirname = path.resolve();
+
 app.use("/api/products", productRoutes);
+
+if (process.env.APP_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("/{*splat}", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
     connectDB();
